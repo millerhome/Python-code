d955f1c217ef653c24190476baec37bb4e4f770a
millerwang <miller680523.public@gmail.com>
2019-08-10 14:34:03 +0800

    本範例會用list、if及wheil技巧，以傳入list及目標參數，找出list中，最接近目標參數的值

--- samplecode/ex1.py
+++ samplecode/ex1.py
@@ -15,6 +15,6 @@ def fun1(list,ss) :
         else:
             i+=1
     return rult
-litAA=[0,5,2,4,6,7,8]
-AA=str(fun1(litAA,6))
+litAA=[0,5,2,4,6,7,8,10]
+AA=str(fun1(litAA,3))
 print('找出list中，最接近目標參數的值:'+AA) 
