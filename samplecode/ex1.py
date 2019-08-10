e2c01c2cd8659bfc066e7a61c9c83c22d0e6f5d7
millerwang <miller680523.public@gmail.com>
2019-08-10 14:20:56 +0800

    OK

---
+++ samplecode/ex1.py
@@ -0,0 +1,20 @@
+#本範例會用list、if及wheil技巧，以傳入list及目標參數，找出list中，最接近目標參數的值
+def fun1(list,ss) :
+    max=len(list)
+    i=1
+    rult=ss
+    while i<max:
+        if list[i]>ss:
+            print('%s %d'%(list[i],rult))
+            if list[i] >rult or list[i]>ss:
+               if rult>ss and rult<list[i]:
+                    rult=rult
+               else :
+                    rult=list[i]
+            i+=1
+        else:
+            i+=1
+    return rult
+litAA=[0,5,2,4]
+AA=str(fun1(litAA,1))
+print('找出list中，最接近目標參數的值:'+AA)
