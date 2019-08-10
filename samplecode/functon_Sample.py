b0d47fc3c6b45a77dae56e7f2e2b28b16dae8862
millerwang <miller680523.public@gmail.com>
2019-08-10 10:58:07 +0800

    OK

--- samplecode/functon_Sample.py
+++ samplecode/functon_Sample.py
@@ -1,7 +1,11 @@
 def Hello(who,SayWorld) :  #定義有多個引數的函式
-    print(who+' '+ SayWorld)
+    print(who+' '+ SayWorld) #用縮排判斷function內需執行程式碼
     print('2 '+who+' '+SayWorld)
 
+def Hello2(who,SaySomething) :
+        sayword=who+SaySomething
+        return ('hello2無言'+' '+sayword,'回傳成功') #函數回傳多個值
 
-
-Hello('miller','hihi')
+Hello('miller','hihi') #呼叫函數1
+s1,s2=Hello2('jerry','HAHA') #呼叫函數2 接一個以上的回傳值
+print(s1,s2)
