4e1091c3233f14cee46ad4f73cefb285d54c44d2
millerwang <miller680523.public@gmail.com>
2019-10-12 14:54:26 +0800

    一般測試

---
+++ test2.py
@@ -0,0 +1,6 @@
+print("Line1")
+x=1+1
+print("Line2")
+first_name=input('Please input first name!')
+last_name=input('Please input Last_name!')
+print(first_name.capitalize() + ' ' + last_name.capitalize())
